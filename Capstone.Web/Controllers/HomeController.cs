8518866c64916899607b8d15668d4602de86b2f4
Eric Warner <ewarner91@gmail.com>
2018-03-28 10:18:02 -0400

    Set up home page dal

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Capstone.Web.DAL;
+using Capstone.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,17 @@ namespace Capstone.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IParkDAL _dal;
+        public HomeController(IParkDAL dal)
+        {
+            _dal = dal;
+        }
 
         // GET: Home
         public ActionResult Index()
         {
-            return View("Index");
+            IList<Park> parks = _dal.GetAllParks();
+            return View("Index", parks);
         }
     }
 }
--- Capstone.Web/Global.asax.cs
+++ Capstone.Web/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Capstone.Web.DAL;
+using Ninject;
+using Ninject.Web.Common;
+using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,12 +11,26 @@ using System.Web.Routing;
 
 namespace Capstone.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    // Setting up the application after including Ninject and Ninject.MVC5
+    // https://stackoverflow.com/questions/47001124/web-api2-ninjectwebcommon-cs-do-not-appear/47002329#47002329
+    public class MvcApplication : NinjectHttpApplication
     {
-        protected void Application_Start()
+        protected override void OnApplicationStarted()
         {
+            base.OnApplicationStarted();
+
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            string connectionString = ConfigurationManager.ConnectionStrings["WeatherDB"].ConnectionString; ; //TODO: Insert Connection String to your database
+
+            kernel.Bind<IParkDAL>().To<ParkDAL>().WithConstructorArgument("connectionString", connectionString);
+
+            return kernel;
         }
     }
 }
