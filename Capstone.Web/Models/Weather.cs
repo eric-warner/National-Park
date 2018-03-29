d88792e5cd5a375febe322ed4581127ae48e0aa1
Eric Warner <ewarner91@gmail.com>
2018-03-29 09:56:02 -0400

    Added advisory property

--- Capstone.Web/Models/Weather.cs
+++ Capstone.Web/Models/Weather.cs
@@ -12,5 +12,66 @@ namespace Capstone.Web.Models
         public int Low { get; set; }
         public int High { get; set; }
         public string Forecast { get; set; }
+
+        public string Advisory
+        {
+            get
+            {
+                bool isRainy = Forecast.ToLower().Equals("rain");
+                bool isPartlyCloudy = Forecast.ToLower().Equals("partly cloudy");
+                bool isThunderstorm = Forecast.ToLower().Equals("thunderstorms");
+                bool isSunny = Forecast.ToLower().Equals("sunny");
+                bool isSnowy = Forecast.ToLower().Equals("snow");
+                bool isCloudy = Forecast.ToLower().Equals("cloudy");
+
+                bool isHot = High > 75;
+                bool isCold = Low < 20;
+                bool isHotAndCold = High - Low > 20;
+
+                string advisory = String.Empty;
+
+                if (isRainy)
+                {
+                    advisory = "Pack rain gear and wear waterproof shoes.";
+                }
+                else if (isPartlyCloudy)
+                {
+                    advisory = "Enjoy the weather.";
+                }
+                else if (isThunderstorm)
+                {
+                    advisory = "Seek shelter and avoid hiking on exposed ridges. ";
+                }
+                else if (isSunny)
+                {
+                    advisory = "Pack sunblock";
+                }
+                else if (isSnowy)
+                {
+                    advisory = "Pack snowshoes.";
+                }
+                else if (isCloudy)
+                {
+                    advisory = "Wear a sweater.";
+                }
+
+                if (isHot)
+                {
+                    advisory += "\nBring extra water.";
+                }
+
+                if (isHotAndCold)
+                {
+                    advisory += "\nWear breathable layers."; 
+                }
+
+                if (isCold)
+                {
+                    advisory += "\nDress warmly.";
+                }
+
+                return advisory;
+            }
+        }
     }
 }
